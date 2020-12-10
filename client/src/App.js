f0d63d68c702fe629e59edde74f4f5bfc0362f63
teresafranxman97 <teresafranxman97@gmail.com>
2020-12-09 23:24:16 -0600

    added routes, attempting to create moviecard

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Route, Link } from 'react-router-dom';
 
+import Movie from './Movies/Movie'
+import MovieCard from './Movies/MovieCard'
+import MovieList from './Movies/MovieList'
 import SavedList from './Movies/SavedList';
 
 export default function App () {
@@ -12,6 +16,7 @@ export default function App () {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
         .then(response => {
+          setMovieList(response.data)
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
         })
@@ -29,8 +34,10 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
+        <div>
+          <Route path='/' component={MovieList} />
+          <Route path='/movies/{id}' render={(props) => <Movie {...props} /> } />
+        </div>
     </div>
   );
 }
